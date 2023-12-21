56701ffebd7e1d638929695eb04942389f84ae83
Md Parvej <mdparvejme24@gmail.com>
2023-12-21 15:41:01 +0600

    install firebase in my applicatio for user login logout authintication system

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1IhBnkkHE5P-uhwY8ePtkA0PZPr7gYrI",
+  authDomain: "taskup-e09bd.firebaseapp.com",
+  projectId: "taskup-e09bd",
+  storageBucket: "taskup-e09bd.appspot.com",
+  messagingSenderId: "605701100771",
+  appId: "1:605701100771:web:ca5467ccafc7e75a4361d6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
