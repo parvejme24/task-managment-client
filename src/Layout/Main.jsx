995576099822878887f18b8b9e146f4eefd37590
Md Parvej <mdparvejme24@gmail.com>
2023-12-21 19:40:40 +0600

    added a navbar

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Navbar from "../Shared/Navbar/Navbar";
+import { NavbarComponent } from "../Shared/Navbar/Navbar";
 import Footer from "../Shared/Footer/Footer";
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
   return (
     <div>
-      <Navbar />
+      <NavbarComponent />
       <Outlet />
       <Footer />
     </div>
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+"use client";
+// import Image from "next/image";
+import { MagnifyingGlass } from "phosphor-react";
+import { Navbar, Button } from "keep-react";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+export const NavbarComponent = () => {
+  const navItems = (
+    <>
+      <Navbar.Link linkName="Home" />
+      <Navbar.Link linkName="Projects" />
+      <Navbar.Link linkName="Blogs" />
+      <Navbar.Link linkName="News" />
+      <Navbar.Link linkName="Resources" />
+    </>
+  );
   return (
-    <div>
-      
+    <div className="shadow-md">
+      <Navbar className="container mx-auto" fluid={true}>
+        <Navbar.Container className="flex items-center justify-between">
+          <Navbar.Container className="flex items-center">
+            <Navbar.Brand>
+              <img src={logo} alt="keep" width="100" height="40" />
+            </Navbar.Brand>
+            <Navbar.Divider></Navbar.Divider>
+            <Navbar.Container
+              tag="ul"
+              className="lg:flex hidden items-center justify-between gap-8"
+            >
+              {navItems}
+            </Navbar.Container>
+            <Navbar.Collapse collapseType="sidebar">
+              <Navbar.Container tag="ul" className="flex flex-col gap-5">
+                {navItems}
+              </Navbar.Container>
+            </Navbar.Collapse>
+          </Navbar.Container>
+
+          <Navbar.Container className="flex gap-2">
+            <Link to="/login">
+              <Button size="sm" type="primary">
+                Login
+              </Button>
+            </Link>
+            <Navbar.Toggle />
+          </Navbar.Container>
+        </Navbar.Container>
+      </Navbar>
     </div>
   );
 };
-
-export default Navbar;
